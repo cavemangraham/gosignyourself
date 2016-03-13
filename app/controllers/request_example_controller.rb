dd32f2a3a975e314ce9b0d52e32334f97750702b
Sean Graham <seanmgrahamnj@yahoo.com>
2016-03-13 04:01:27 -0400

    styling and trying to redirect on post

--- app/controllers/request_example_controller.rb
+++ app/controllers/request_example_controller.rb
@@ -1,11 +1,8 @@
 class RequestExampleController < ActionController::Base
-  def curl_get_example
-    render text: 'Thanks for sending a GET request with cURL!'
-  end
-
   def curl_post_example
-    render text: "Thanks for sending a POST request with cURL! Payload: #{request.body.read}"
     request.body.rewind
     $sign_variable = "#{request.body.read}"
+
+    render action: "index"
   end
 end
