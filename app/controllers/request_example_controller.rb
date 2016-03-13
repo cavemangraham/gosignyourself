b17b15a99426ae837d7f4fa15013a4561c274ff5
Sean Graham <seanmgrahamnj@yahoo.com>
2016-03-13 05:20:48 -0400

    no more render 505 errors

--- app/controllers/request_example_controller.rb
+++ app/controllers/request_example_controller.rb
@@ -2,7 +2,5 @@ class RequestExampleController < ActionController::Base
   def curl_post_example
     request.body.rewind
     $sign_variable = "#{request.body.read}"
-
-    render action: "index"
   end
 end
